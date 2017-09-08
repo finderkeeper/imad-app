cf9f854f16d6ef3685071f82e0ca2e0ecae7ca9b
finderkeeper <30702330+finderkeeper@users.noreply.github.com>
2017-09-09 00:43:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.sendFile('Article one is here and will soon be served');
+    res.send('Article one is here and will soon be served');
 });
 
 app.get('/article-two', function(req,res){
-    res.sendFile('Article two is here and will soon be served');
+    res.send('Article two is here and will soon be served');
 });
 
 app.get('/article-three', function(req,res){
-    res.sendFile('Article three is here and will soon be served');
+    res.send('Article three is here and will soon be served');
 });
 
 app.get('/ui/style.css', function (req, res) {
